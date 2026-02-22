aea6334b5c019df3e64d778494f56fc7adfe6ce0
layter92-netizen <layter92@gmail.com>
2026-02-22 16:59:11 +0200

    Add files via upload

---
+++ reports/registry.js
@@ -0,0 +1,11 @@
+const reportsData = [
+    {
+        "id": "global-hazelnut-price-2026",
+        "title": "Global Hazelnut Price Cycles",
+        "date": "2026-02-21",
+        "category": "АНАЛІТИКА",
+        "description": "Глибокий аналіз циклічності цін на світовому ринку фундука. Прогнози на сезон 2026/27 на основі даних NotebookLM.",
+        "filename": "Global_Hazelnut_Price_Cycles.pdf",
+        "type": "Whitepaper"
+    }
+];
